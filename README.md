e7c023819dee0ba779eca0ce83db42ebbf0df8df
Sean Dou <douguohui@gmail.com>
2016-08-04 13:08:10 +0800

    compatible with node6

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # node-next
 
 Using babel to bring more es-next features to Node.
+
+## References
+
+- [ECMAScript 6 compatibility table](http://kangax.github.io/compat-table/es6/)
--- index.js
+++ index.js
@@ -4,15 +4,20 @@ let plugins = [
   'syntax-async-functions',
   'syntax-async-generators',
   'transform-async-to-generator',
-  'transform-es2015-destructuring',
   'transform-es2015-modules-commonjs',
-  'transform-es2015-parameters',
   'transform-es2015-sticky-regex',
   'transform-es2015-unicode-regex'
 ];
 
 if (process.version.match(/v4/)) {
   plugins.push('transform-es2015-spread');
+  plugins.push('transform-es2015-destructuring');
+  plugins.push('transform-es2015-parameters');
+}
+
+if (process.version.match(/v5/)) {
+  plugins.push('transform-es2015-destructuring');
+  plugins.push('transform-es2015-parameters');
 }
 
 require('babel-core/register')({
